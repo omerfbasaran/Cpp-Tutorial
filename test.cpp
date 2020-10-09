bd5868df147d16153bfb7a4e9604e87a0d7d8a1d
OFB <omerfbasaran@gmail.com>
2020-10-09 17:40:04 +0300

    first commit

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    int x=13;
+    bool a=true;
+    if (x<=15 && a)
+    cout<<"x 15'den kucuk veya esit ve a true";
+
+    //hhh
+
+}
