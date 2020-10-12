8214878e58ff3612c2dc3abb6bad1369d19562f3
OFB <omerfbasaran@gmail.com>
2020-10-12 16:41:12 +0300

    consexpr

--- test.cpp
+++ test.cpp
@@ -5,10 +5,10 @@ int main ()
    /* int x=13;
     bool a=true;
     if (x<=15 && a)
-    cout<<"x 15'den kucuk veya esit ve a true";*/
+    cout<<"x 15'den kucuk veya esit ve a true";
 
 ////////////////////////////////////////////////
-/*int x=1;
+int x=1;
 switch (x)
 {
 case 1:
@@ -21,21 +21,23 @@ case 3:
     cout << "x="<<x;
 default:
     break;
-}*/
+}
 
-/*for (auto i = 0; i < 21; i++)
+for (auto i = 0; i < 21; i++)
 {
     cout << "sayac="<<i<<endl;
-}*/
-/*int x=0;
+}
+int x=0;
 while (x<16)
 {
     cout << "Sayac = "<<x<<endl;
     x++;
-}*/
+}
 const int a=5;
-int array[a]={0,22,32,34,43};
+int array[a]={0,22,32,34,43};*/
 
-
+const int n=241;
+constexpr n2=n;
+cout << n2;
 
 }
