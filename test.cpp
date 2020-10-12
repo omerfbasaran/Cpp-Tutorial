9be16e7763479278da0672fdbef1773a05a990db
OFB <omerfbasaran@gmail.com>
2020-10-12 14:15:10 +0300

    switch

--- test.cpp
+++ test.cpp
@@ -3,11 +3,28 @@
 using namespace std;
 int main ()
 {
-    int x=13;
+   /* int x=13;
     bool a=true;
     if (x<=15 && a)
-    cout<<"x 15'den kucuk veya esit ve a true";
+    cout<<"x 15'den kucuk veya esit ve a true";*/
 
-    //test_4
+////////////////////////////////////////////////
+int x=1;
+switch (x)
+{
+case 1:
+    cout << "x= "<<x;
+    x++;
+    break;
+case 2:
+    cout << "x= "<<x;
+    x++;
+    break;
+case 3:
+    cout << "x= "<<x;
+    break;
+default:
+    break;
+}
 
 }
