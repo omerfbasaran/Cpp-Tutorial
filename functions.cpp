d0fd2652f2b86aae53b09d8a82f0acbfe2931d14
OFB <omerfbasaran@gmail.com>
2020-10-12 17:45:57 +0300

    float sonuc

--- functions.cpp
+++ functions.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 using namespace std;
-int kare(int x);
+//int kare(int x);
+float karef(float y);
 int main()
 {
-    int y;
+    /*int y;
     cout <<"Sayi giriniz= ";
     cin >> y ;
     int sonuc;
     sonuc= kare(y);
-    cout <<"sonuc= "<<sonuc; 
+    cout <<"sonuc= "<<sonuc; */
+    float y;
+    cout << "Ondalikli sayi girin-->";
+    cin >> y;
+    float floatsonuc;
+    floatsonuc = karef(y);
+    cout <<"Float sonuc="<<floatsonuc;
+    
 }
-int kare(int x)
+/*int kare(int x)
     {
         return x * x;
-    }
+    }*/
+float karef (float y)
+{
+    return y*y;
+}
