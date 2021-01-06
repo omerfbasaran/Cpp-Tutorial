792d391555e60ea41322d473d3e3e1d6e232a29c
Ömer Faruk <72097255+omerfbasaran@users.noreply.github.com>
2021-01-06 03:32:12 +0300

    Delete Display.h

--- C/Display.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DISPLAY_H
-#define __DISPLAY_H
-#include <stdint.h>
-	void display_init(void);
-	void dipslay_power_on(void);
-	void dipslay_power_off(void);
-	void dipslay_write_to_screen(int32_t num);
-
-#endif
